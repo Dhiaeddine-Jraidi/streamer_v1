414af0f9590688be2dbfb46044e1f36553271a9c
Dhiaeddine Jraidi <130515774+Dhiaeddine-Jraidi@users.noreply.github.com>
2024-06-03 19:44:17 +0100

    Update file_listing.php

--- file_listing.php
+++ file_listing.php
@@ -1,31 +1,32 @@
 <?php
+include 'parse_logs.php';
 $directory = '/var/www/html/drive/';
-$files = scandir($directory);
-foreach ($files as $file) {
-    if ($file != '.' && $file != '..' && $file[0] != '.') {
-        $fileSize = filesize($directory . '/' . $file);
-        echo '<li class="file">
-                <a href="drive/' . $file . '">' . $file . '</a> <span>(' . formatSizeUnits($fileSize) . ')</span>
-              </li>';
+$series = scandir($directory);
+echo '<div class="server-directory">';
+foreach ($series as $serie) {
+    if ($serie != '.' && $serie != '..' && $serie[0] != '.' && is_dir($directory . $serie)) {
+        $coverPath = $directory . $serie . '/.cover.jpg';
+        $coverUrl = file_exists($coverPath) ? 'drive/' . $serie . '/.cover.jpg' : 'default-cover.jpg';
+        $lastWatchedfile = getLastWatchedEpisode($serie);
+        if ($lastWatchedfile === null) {
+            $files = scandir($directory . $serie);
+            $files = array_filter($files, function($file) {
+                return $file[0] !== '.';
+            });
+            $files = array_values($files);
+            $firstFile = reset($files);
+            $newWatchedfile = $firstFile;
+        } else {
+            $newWatchedfile = go_next_episode_or_stay_in_the_same($lastWatchedfile, $serie);
+        }
+        $url = 'drive/' . $serie . '/' . $newWatchedfile;
+        echo '<div class="folder-block">
+                <a href="' . $url . '">
+                    <img src="' . $coverUrl . '" alt="' . $serie . ' cover" class="cover-image">
+                    <div class="folder-name">' . $serie . '</div>
+                </a>
+              </div>';
     }
 }
-
-function formatSizeUnits($bytes)
-{
-    if ($bytes >= 1073741824) {
-        $bytes = number_format($bytes / 1073741824, 2) . ' GB';
-    } elseif ($bytes >= 1048576) {
-        $bytes = number_format($bytes / 1048576, 2) . ' MB';
-    } elseif ($bytes >= 1024) {
-        $bytes = number_format($bytes / 1024, 2) . ' KB';
-    } elseif ($bytes > 1) {
-        $bytes = $bytes . ' bytes';
-    } elseif ($bytes == 1) {
-        $bytes = $bytes . ' byte';
-    } else {
-        $bytes = '0 bytes';
-    }
-
-    return $bytes;
-}
+echo '</div>';
 ?>
