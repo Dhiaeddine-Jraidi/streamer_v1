a5cf5253c6655affc2c83053d6cc9ea8554d6ab0
Dhiaeddine Jraidi <130515774+Dhiaeddine-Jraidi@users.noreply.github.com>
2024-06-15 16:33:21 +0100

    Update vtt_cleaner.py

--- vtt_cleaner.py
+++ vtt_cleaner.py
@@ -4,10 +4,8 @@ import pandas as pd
 def parse_vtt_file(file_path):
     timeframes = []
     dialogues = []
-
     with open(file_path, 'r', encoding='utf-8') as file:
         lines = file.readlines()
-
     i = 0
     while i < len(lines):
         line = lines[i].strip()
@@ -27,16 +25,17 @@ def parse_vtt_file(file_path):
         'timeframe': timeframes,
         'dialogue': dialogues
     })
-
     return df
 
 
 
-if __name__ == "__main__":
-    file_path = sys.argv[1]
-    df = parse_vtt_file(file_path)
+def cleaned_df(df):
     df = df[df['dialogue'].str.contains(r'[\u0600-\u06FF]', regex=True)]
-    df = df[["timeframe","dialogue"]]
+    df = df.groupby('timeframe', as_index=False).agg({'dialogue': lambda x: r'\N'.join(x)})
+    return df
+
+
+def create_cleaned_vtt(df):
     vtt_content = "WEBVTT\n\n"
     for index, row in df.iterrows():
         timeframe = row['timeframe']
@@ -45,3 +44,10 @@ if __name__ == "__main__":
 
     with open(file_path, 'w', encoding='utf-8') as file:
         file.write(vtt_content)
+
+
+if __name__ == "__main__":
+    file_path = sys.argv[1]
+    df = parse_vtt_file(file_path)
+    df = cleaned_df(df)
+    create_cleaned_vtt(df)
